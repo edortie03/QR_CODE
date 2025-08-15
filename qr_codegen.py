3d98bf763d640be44b8abb7e7a87009ca7429f7f
Edward Denis <edorward.21@mail.com>
2025-08-15 16:56:14 +0300

    files commited

---
+++ qr_codegen.py
@@ -0,0 +1,22 @@
+import qrcode
+
+def gen_qr(data, file_name):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="orange", back_color="black")
+    img.save(file_name)
+
+    print(f"QR code generated and saved as {file_name}")
+
+# Example usage
+data = "https://github.com/edortie03"
+file_name = "edor_github.png"
+gen_qr(data, file_name)
+# This code generates a QR code for the given data and saves it as an image file.
